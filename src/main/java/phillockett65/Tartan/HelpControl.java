42ba084a1c5eb92309f7a4caa1e3dc2361e8a7f1
Phil Lockett <phillockett65@gmail.com>
2024-12-31 18:13:04 -0500

    Enbedded the User Guide as Text and TextFlows instead of using HTML and a WebEngine.

--- src/main/java/phillockett65/Tartan/HelpControl.java
+++ src/main/java/phillockett65/Tartan/HelpControl.java
@@ -28,16 +28,22 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -61,6 +67,188 @@ public class HelpControl extends Stage {
     private double y = 0.0;
 
     private boolean result = false;
+
+
+
+    /************************************************************************
+     * Support code for the Embedded Help page.
+     */
+
+    private class BodyFlow extends TextFlow {
+        public BodyFlow(Text... text) {
+            super(text);
+
+            this.setTextAlignment(TextAlignment.JUSTIFY);
+        }
+
+    }
+    private class Body extends Text {
+        public Body(String text) {
+            super(text);
+
+            this.setFont(Font.font("Arial", FontWeight.NORMAL, 17));
+            this.setWrappingWidth(475);
+
+            final Color fore = Color.web("#e8e8e8");
+            this.setFill(fore);
+        }
+
+    }
+
+    private class H1Flow extends BodyFlow {
+        public H1Flow(Text... text) {
+            super(text);
+
+            this.setPadding(new Insets(0, 0, 12, 0));
+        }
+
+    }
+    private class H1 extends Body {
+        public H1(String text) {
+            super(text);
+            this.setFont(Font.font("Arial", FontWeight.BOLD, 28));
+        }
+    }
+
+    private class H2Flow extends BodyFlow {
+        public H2Flow(Text... text) {
+            super(text);
+
+            this.setPadding(new Insets(8, 0, 10, 0));
+        }
+
+    }
+    private class H2 extends Body {
+        public H2(String text) {
+            super(text);
+            this.setFont(Font.font("Arial", FontWeight.BOLD, 22));
+        }
+    }
+
+    private class H3Flow extends BodyFlow {
+        public H3Flow(Text... text) {
+            super(text);
+
+            this.setPadding(new Insets(8, 0, 10, 0));
+        }
+
+    }
+    private class H3 extends Body {
+        public H3(String text) {
+            super(text);
+            this.setFont(Font.font("Arial", FontWeight.BOLD, 19));
+        }
+    }
+
+    private class PFlow extends BodyFlow {
+        public PFlow(Text... text) {
+            super(text);
+
+            this.setPadding(new Insets(0, 0, 16, 0));
+            this.setPrefWidth(470);
+            this.setMaxWidth(470);
+        }
+
+    }
+    private class P extends Body {
+        public P(String text) {
+            super(text);
+            this.setFont(Font.font("Verdana", FontWeight.NORMAL, 16));
+            }
+    }
+    private class Code extends Body {
+        public Code(String text) {
+            super(text);
+            this.setFont(Font.font("Courier New", FontWeight.NORMAL, 16));
+            }
+    }
+    private class Bold extends Body {
+        public Bold(String text) {
+            super(text);
+            this.setFont(Font.font("Verdana", FontWeight.BOLD, 16));
+            }
+    }
+
+    private class ListFlow extends BodyFlow {
+        public ListFlow(Text... text) {
+            super(text);
+
+            this.setPadding(new Insets(0, 0, 16, 30));
+            this.setPrefWidth(440);
+            this.setMaxWidth(440);
+        }
+
+    }
+    private class ListItem extends Body {
+        public ListItem(String text) {
+            super(text);
+            this.setFont(Font.font("Verdana", FontWeight.NORMAL, 16));
+            }
+    }
+
+    VBox help = new VBox(
+        new H1Flow(new H1("Tartan User Guide")),
+        new H2Flow(new H2("\"Tartan Designer\" Window")),
+        new H3Flow(new H3("Colours")),
+        new PFlow(
+            new P("'Tartan' is an application for designing Tartan patterns using colours from a configurable \"Colour Palette\". "),
+            new P("Each colour in the \"Colour Palette\" can be selected then changed using the \"Selected Colour\" control. "),
+            new P("Each colour swatch can be labelled using the adjoining text field. ")
+            ),
+        new H3Flow(new H3("Layout")),
+        new PFlow(
+            new P("The pattern layout can be controlled in a number of ways. "),
+            new P("\"Tartan\" uses a pattern that repeats after an specific number of threads. "),
+            new P("\"Column Repeat Count\" defines that number of vertical threads. "),
+            new P("Typically the pattern is the same for the vertical and horizontal threads and the \"Duplicate the Column colours for the Rows\" check box performs this duplication automatically. "),
+            new P("However, if this is not desired, uncheck the box and set the vertical and horizontal thread colours independently. "),
+            new P("This also allows the number of horizontal threads (before repeating) to be set independently. ")
+            ),
+        new PFlow(
+            new P("Guide lines divide the \"Sample\" window into eight equal parts vertically and horizontally. "),
+            new P("The \"Show Guide Lines\" check box controls whether these are displayed or not. "),
+            new P("The colour of the guide lines "),
+            new Bold("AND "),
+            new P("the colour of thread highlights can be changed using the colour selector labelled \"Guide Line Colour\". ")
+            ),
+        new PFlow(
+            new P("Typically multiple adjacent treads have the same colour. "),
+            new P("\"Thread Repeat Count\" colours up to eight adjacent threads using the currently selected colour. ")
+            ),
+        new PFlow(
+            new P("The size of the threads can be increased using the \"Thread Size\" control, to make life easier during the design phase, however this will effect the generated graphic when saved. "),
+            new P("The thickness of the thread border can be adjusted using the \"Thread Border Thickness\" control, if needed. ")
+            ),
+        new H3Flow(new H3("Pull-Down Menu")),
+        new PFlow(
+            new P("The Pull-Down Menu allows tartan designs to be loaded from and saved to the "),
+            new Code("Tartan/swatches "),
+            new P("directory. "),
+            new P("Each tartan design is saved in it's own subdirectory as a settings file and includes a .png image file showing the design repeated in a 2 by 2 layout. ")
+            ),
+        new H2Flow(new H2("\"Sample\" Window")),
+        new H3Flow(new H3("Colouring")),
+        new PFlow(
+            new P("Across the top edge and down the left side of the \"Sample\" window are extended threads which are sensitive to mouse movement and clicking. "),
+            new P("Moving the mouse pointer over these areas will cause threads to be highlighted based on the current selections. "),
+            new P("Clicking on these areas will colour the highlighted threads using the currently selected colour from the \"Colour Palette\". ")
+            ),
+        new H3Flow(new H3("Adjusting")),
+        new PFlow(
+            new P("Additionally, threads can be inserted or deleted. "),
+            new P("With the focus on the \"Sample\" window, holding down the \"alt\" or \"ctrl\" key will display a message in the \"Sample\" heading bar to indicate functional options: ")
+            ),
+        new ListFlow(
+            new ListItem("With the \"alt\" key held down, clicking on the extended threads will "),
+            new Bold("delete "),
+            new ListItem("the highlighted number of threads. ")
+            ),
+        new ListFlow(
+            new ListItem("With the \"ctrl\" key held down, clicking on the extended threads will "),
+            new Bold("insert "),
+            new ListItem("the highlighted number of threads. ")
+            )
+    );
 
 
 
@@ -116,10 +304,11 @@ public class HelpControl extends Stage {
         return topBar;
     }
 
+
     /**
-     * Builds the options buttons as a HBox and includes the action event 
-     * handlers for both the done and clear buttons.
-     * @return the HBox that represents the options buttons.
+     * Builds the options button as a HBox and includes the action event 
+     * handler for cancel button.
+     * @return the HBox that represents the options button.
      */
     private HBox buildOptions() {
         HBox options = new HBox();
@@ -140,19 +329,26 @@ public class HelpControl extends Stage {
         return options;
     }
 
+
     /**
-     * Builds a WebView and loads it with "guide.html".
-     * @return the WebView loaded with "guide.html".
+     * Builds a ScrollPane and fills it with "help".
+     * @return the ScrollPane filled with "help".
      */
-    private WebView buildWebView() {
-        WebView webView = new WebView();
-        webView.setPrefSize(500, 600);
+    private ScrollPane buildHelpPage() {
 
-        WebEngine engine = webView.getEngine();
-        engine.load("file://" + getClass().getResource("guide.html").getFile());
+        final Color back = Color.web("#101010");
+        help.setBackground(Background.fill(back));
+        // help.setPrefWidth(470);
+        help.setPadding(new Insets(4, 16, 4, 4));
 
-        return webView;
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setMaxSize(500, 600);
+        scrollPane.setContent(help);
+        // scrollPane.setPadding(new Insets(10));
+
+        return scrollPane;
     }
+
 
     /**
      * Builds the User controls as a VBox.
@@ -164,7 +360,7 @@ public class HelpControl extends Stage {
         panel.setSpacing(10);
         panel.setPadding(new Insets(10.0));
 
-        panel.getChildren().add(buildWebView());
+        panel.getChildren().add(buildHelpPage());
         panel.getChildren().add(buildOptions());
 
         return panel;
@@ -197,20 +393,12 @@ public class HelpControl extends Stage {
      */
 
     /**
-     * Synchronise all controls with the model.
-     */
-    private void syncUI() {
-    }
-
-
-    /**
      * Constructor.
      */
     public HelpControl(String title) {
         super();
 
         init(title);
-        syncUI();
     }
 
 
@@ -219,7 +407,6 @@ public class HelpControl extends Stage {
      * @return true if the control was updated, false if cancelled.
      */
     public boolean showControl() {
-        syncUI();
         this.showAndWait();
 
         return result;
